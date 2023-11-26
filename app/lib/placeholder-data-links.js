e30231bb09125a4c0fa971932bd8bdec2fe47dfc
ArkalKeirosRas <recargamarcas6@gmail.com>
2023-11-25 20:30:11 -0600

    cartas genero femenino

--- app/lib/placeholder-data-links.js
+++ app/lib/placeholder-data-links.js
@@ -5,9 +5,9 @@ const links = [
         description: 'cartas estilizadas para el genero masculino'
     },
     {
-        route: 'https://nextfjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
-        title: 'Docs',
-        description: ' Find in-depth information about Next.js features and API.'
+        route: 'https://docs.google.com/document/d/1YAgHHH3n_gLuJNFH7ycJnkpPvs1aHpwH/edit?usp=sharing&ouid=110522811894200833300&rtpof=true&sd=true',
+        title: 'Cartas de recomendación femenina',
+        description: 'cartas estilizadas para el genero femenino'
     },
     {
         route: 'https://nextdjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
