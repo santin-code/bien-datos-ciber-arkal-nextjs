bb548e3c378b1cd1818f9888a9b40fa931e06352
ArkalKeirosRas <recargamarcas6@gmail.com>
2023-11-25 20:09:18 -0600

    creando datos

---
+++ app/lib/placeholder-data-links.js
@@ -0,0 +1,21 @@
+const links = [
+    {
+        route: 'https://docs.google.com/document/d/17o2ygiut1MfPxGOWvAPtEaEvX01ffYMP/edit?usp=sharing&ouid=110522811894200833300&rtpof=true&sd=true',
+        title: 'Cartas de recomendación masculino',
+        description: 'cartas estilizadas para el genero masculino'
+    },
+    {
+        route: 'https://nextfjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+        title: 'Docs',
+        description: ' Find in-depth information about Next.js features and API.'
+    },
+    {
+        route: 'https://nextdjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+        title: 'Docs',
+        description: ' Find in-depth information about Next.js features and API.'
+    }
+
+];
+module.exports = {
+    links
+}
